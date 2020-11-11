26420a952906198d0236b6be1c9b8791fd29364f
Felipe Bueno <felipebueno181@gmail.com>
2020-11-11 18:29:45 -0300

    load balancer com ribbon logger para validar

--- hr-worker/src/main/java/br/com/felipebueno/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/br/com/felipebueno/hrworker/controllers/WorkerController.java
@@ -2,8 +2,10 @@ package br.com.felipebueno.hrworker.controllers;
 
 import br.com.felipebueno.hrworker.entities.Worker;
 import br.com.felipebueno.hrworker.repositories.WorkerRepository;
-import org.hibernate.jdbc.Work;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,11 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerController {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -27,6 +34,9 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Worker list = repository.findById(id).get();
         return ResponseEntity.ok(list);
     }
